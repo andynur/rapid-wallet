709b20187e7daec1f9b2ab60bf36306651f928ec
Andy Nur <andynur.id@gmail.com>
2024-07-17 14:32:31 +0700

    init project

---
+++ resources/js/Pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import BlocksLayout from '@/Layouts/BlocksLayout';
+import { PageProps } from '@/types';
+
+export default function Dashboard({ auth }: PageProps) {
+    return (
+        <BlocksLayout />
+    );
+}
